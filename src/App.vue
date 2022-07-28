bfbd02af7e1195a67ea0f64aa3f0226eba059648
shadycheer <huangfeifan@ones.ai>
2022-07-29 01:11:46 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="less" scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  &__vue {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+
+    &:hover {
+      filter: drop-shadow(0 0 2em #42b883aa);
+    }
+  }
+}
+</style>
